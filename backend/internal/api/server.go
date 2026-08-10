73c1680dd15a950120600ea6a758700cbef06dc0
X-Calibre <59793632+X-Calibre@users.noreply.github.com>
2026-08-10 07:15:38 +0200

    Expose shared preferences API

--- backend/internal/api/server.go
+++ backend/internal/api/server.go
@@ -12,35 +12,33 @@ import (
 )
 
 type Server struct {
-	httpServer *http.Server
-	logger     *slog.Logger
-	playback   *playback.Manager
-	streams    *stream.Store
-	favourites *storage.Favourites
+	httpServer  *http.Server
+	logger      *slog.Logger
+	playback    *playback.Manager
+	streams     *stream.Store
+	favourites  *storage.Favourites
+	preferences *storage.Preferences
 }
 
-// New creates a new HTTP server.
 func New(
 	addr string,
 	logger *slog.Logger,
 	playback *playback.Manager,
 	streams *stream.Store,
 	favourites *storage.Favourites,
+	preferences *storage.Preferences,
 	frontend string,
 ) *Server {
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir(frontend))
 
 	server := &Server{
-		logger:     logger,
-		playback:   playback,
-		streams:    streams,
-		favourites: favourites,
-		httpServer: &http.Server{
-			Addr:    addr,
-			Handler: mux,
-		},
+		logger:      logger,
+		playback:    playback,
+		streams:     streams,
+		favourites:  favourites,
+		preferences: preferences,
+		httpServer: &http.Server{Addr: addr, Handler: mux},
 	}
 
 	mux.HandleFunc("/api/player/play", server.play)
@@ -49,40 +47,28 @@ func New(
 	mux.HandleFunc("/api/player/volume", server.volume)
 	mux.HandleFunc("/api/streams", server.streamsList)
 	mux.HandleFunc("/api/favourites", server.favouritesHandler)
+	mux.HandleFunc("/api/preferences", server.preferencesHandler)
 	mux.HandleFunc("/api/catalogue/update", server.updateCatalogue)
 	mux.HandleFunc("/api/version", server.version)
-
 	mux.Handle("/", fileServer)
 
 	return server
 }
 
 func (s *Server) Start() error {
-	s.logger.Info(
-		"Starting HTTP server",
-		"address", s.httpServer.Addr,
-	)
-
+	s.logger.Info("Starting HTTP server", "address", s.httpServer.Addr)
 	err := s.httpServer.ListenAndServe()
-
 	if err == http.ErrServerClosed {
 		return nil
 	}
-
 	return err
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
 	s.logger.Info("Stopping HTTP server")
-
 	return s.httpServer.Shutdown(ctx)
 }
 
 func (s *Server) home(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte(
-		version.AppName +
-			" is running\nVersion: " +
-			version.Version +
-			"\n",
-	))
+	_, _ = w.Write([]byte(version.AppName + " is running\nVersion: " + version.Version + "\n"))
 }

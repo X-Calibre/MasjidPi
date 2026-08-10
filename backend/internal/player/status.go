56a1580fb1f3912642063897d5de5ef740ad4d7d
X-Calibre <59793632+X-Calibre@users.noreply.github.com>
2026-08-10 06:51:47 +0200

    Add audio device player types

--- backend/internal/player/status.go
+++ backend/internal/player/status.go
@@ -1,9 +1,15 @@
 package player
 
 type Status struct {
-	Version string `json:"version"`
-	State   string `json:"state"`
-	URL     string `json:"url"`
-	Volume  int    `json:"volume"`
-	Paused  bool   `json:"paused"`
+	Version     string `json:"version"`
+	State       string `json:"state"`
+	URL         string `json:"url"`
+	Volume      int    `json:"volume"`
+	Paused      bool   `json:"paused"`
+	AudioDevice string `json:"audio_device"`
+}
+
+type AudioDevice struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }

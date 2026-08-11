f3f6aa814e8cdb05290e0753f53af6c32a0ac24e
X-Calibre <59793632+X-Calibre@users.noreply.github.com>
2026-08-11 12:19:26 +0200

    fix: recover MPV process after unexpected exit

--- backend/internal/player/process.go
+++ backend/internal/player/process.go
@@ -1,55 +1,120 @@
 package player
 
 import (
+	"errors"
 	"os"
 	"os/exec"
+	"sync"
 )
 
 type Process struct {
+	mu     sync.Mutex
 	socket string
 	cmd    *exec.Cmd
+	done   chan struct{}
 }
 
 func NewProcess(socket string) *Process {
-	return &Process{
-		socket: socket,
-		cmd: exec.Command(
-			"mpv",
-			"--idle=yes",
-			"--no-video",
-			"--no-ytdl",
-			"--really-quiet",
-			"--terminal=no",
-			"--ao=alsa",
-			"--input-ipc-server="+socket,
-		),
-	}
+	return &Process{socket: socket}
 }
 
 func (p *Process) Start() error {
+	p.mu.Lock()
+	if p.cmd != nil {
+		select {
+		case <-p.done:
+			p.cmd = nil
+			p.done = nil
+		default:
+			p.mu.Unlock()
+			return nil
+		}
+	}
+
 	// Remove any stale socket from a previous crash.
 	_ = os.Remove(p.socket)
 
+	cmd := exec.Command(
+		"mpv",
+		"--idle=yes",
+		"--no-video",
+		"--no-ytdl",
+		"--really-quiet",
+		"--terminal=no",
+		"--ao=alsa",
+		"--input-ipc-server="+p.socket,
+	)
+
 	// MPV runs as a background service. Do not forward its terminal/progress
 	// output to MasjidPi's stdout/stderr and therefore into journald.
-	p.cmd.Stdout = nil
-	p.cmd.Stderr = nil
+	cmd.Stdout = nil
+	cmd.Stderr = nil
 
-	return p.cmd.Start()
+	if err := cmd.Start(); err != nil {
+		p.mu.Unlock()
+		return err
+	}
+
+	done := make(chan struct{})
+	p.cmd = cmd
+	p.done = done
+	p.mu.Unlock()
+
+	go func() {
+		_ = cmd.Wait()
+		close(done)
+	}()
+
+	return nil
 }
 
 func (p *Process) Stop() error {
-	if p.cmd == nil || p.cmd.Process == nil {
+	p.mu.Lock()
+	cmd := p.cmd
+	done := p.done
+	p.mu.Unlock()
+
+	if cmd == nil || cmd.Process == nil {
 		return nil
 	}
 
-	err := p.cmd.Process.Kill()
+	err := cmd.Process.Kill()
+	if err != nil && !errors.Is(err, os.ErrProcessDone) {
+		return err
+	}
 
-	_ = p.cmd.Wait()
-
+	<-done
 	_ = os.Remove(p.socket)
 
-	p.cmd = nil
+	p.mu.Lock()
+	if p.cmd == cmd {
+		p.cmd = nil
+		p.done = nil
+	}
+	p.mu.Unlock()
 
-	return err
+	return nil
+}
+
+func (p *Process) Restart() error {
+	if err := p.Stop(); err != nil {
+		return err
+	}
+	return p.Start()
+}
+
+func (p *Process) Exited() bool {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if p.cmd == nil {
+		return true
+	}
+
+	select {
+	case <-p.done:
+		return true
+	default:
+		return false
+	}
 }

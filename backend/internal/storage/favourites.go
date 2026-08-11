8e77d2144b053d8432da2c190163e0d9f0ccad86
X-Calibre <59793632+X-Calibre@users.noreply.github.com>
2026-08-11 11:42:44 +0200

    Harden favourites persistence with atomic writes

--- backend/internal/storage/favourites.go
+++ backend/internal/storage/favourites.go
@@ -44,5 +44,25 @@ func (f *Favourites) Save(ids []string) error {
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(f.path, data, 0644)
+
+	tmp, err := os.CreateTemp(filepath.Dir(f.path), ".favourites-*.tmp")
+	if err != nil {
+		return err
+	}
+	tmpName := tmp.Name()
+	defer os.Remove(tmpName)
+
+	if err := tmp.Chmod(0600); err != nil {
+		_ = tmp.Close()
+		return err
+	}
+	if _, err := tmp.Write(data); err != nil {
+		_ = tmp.Close()
+		return err
+	}
+	if err := tmp.Close(); err != nil {
+		return err
+	}
+
+	return os.Rename(tmpName, f.path)
 }

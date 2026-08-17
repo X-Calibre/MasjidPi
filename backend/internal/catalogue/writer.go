ed8bb11a399f247f21edd20ffc24abb5f7ed5de2
X-Calibre <59793632+X-Calibre@users.noreply.github.com>
2026-08-17 20:13:12 +0200

    Optimise catalogue persistence writes

--- backend/internal/catalogue/writer.go
+++ backend/internal/catalogue/writer.go
@@ -1,18 +1,50 @@
 package catalogue
 
 import (
+	"bytes"
 	"encoding/json"
 	"os"
+	"path/filepath"
 
 	"github.com/X-Calibre/MasjidPi/backend/internal/stream"
 )
 
 func WriteCatalogue(filename string, streams []stream.Stream) error {
-
 	data, err := json.MarshalIndent(streams, "", "    ")
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile(filename, data, 0644)
+	if existing, err := os.ReadFile(filename); err == nil {
+		if bytes.Equal(existing, data) {
+			return nil
+		}
+	} else if !os.IsNotExist(err) {
+		return err
+	}
+
+	if err := os.MkdirAll(filepath.Dir(filename), 0755); err != nil {
+		return err
+	}
+
+	tmp, err := os.CreateTemp(filepath.Dir(filename), ".catalogue-*.tmp")
+	if err != nil {
+		return err
+	}
+	tmpName := tmp.Name()
+	defer os.Remove(tmpName)
+
+	if err := tmp.Chmod(0644); err != nil {
+		_ = tmp.Close()
+		return err
+	}
+	if _, err := tmp.Write(data); err != nil {
+		_ = tmp.Close()
+		return err
+	}
+	if err := tmp.Close(); err != nil {
+		return err
+	}
+
+	return os.Rename(tmpName, filename)
 }

4b238080222d39bcef40e18667a8f6fdb3c3949d
X-Calibre <59793632+X-Calibre@users.noreply.github.com>
2026-09-12 17:51:41 +0200

    Add boot entries for both Pi 3 system slots

--- appliance-image/image/pi3-ab/setup.sh
+++ appliance-image/image/pi3-ab/setup.sh
@@ -7,7 +7,6 @@ component=$1
 case "$component" in
    SYSTEM)
       cat > "$IMAGEMOUNTPATH/etc/fstab" <<'EOF'
-/dev/mmcblk0p2  /               ext4  rw,relatime,errors=remount-ro,commit=30  0  1
 /dev/mmcblk0p1  /boot/firmware  vfat  defaults,rw,noatime,errors=remount-ro     0  2
 /dev/mmcblk0p4  /persistent     ext4  defaults,rw,noatime                       0  2
 EOF
